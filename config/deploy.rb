6a22401e54937da828299c912efd34e3ad027fc5
Your Name <you@example.com>
2015-05-06 14:13:16 +0000

    ddd

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'laravel'
-set :repo_url, 'https://github.com/ravaboard/capistranas.git'
+set :repo_url, 'https://github.com/ravaboard/lara.git'
 set :deploy_to, '/var/www'
 # set:release_path,  { File.join(releases_path, release_name) }
 set :scm, :git
@@ -24,12 +24,8 @@ namespace :deploy do
                 execute :chmod, "u+x artisan"
 				execute :php, "artisan clear-compiled"
                 execute :php, "artisan optimize"
-				execute :chmod, "-R 777 app/storage/cache"
-                execute :chmod, "-R 777 app/storage/logs"
-                execute :chmod, "-R 777 app/storage/meta"
-                execute :chmod, "-R 777 app/storage/sessions"
-                execute :chmod, "-R 777 app/storage/views"
-				execute :php, "artisan migrate" # run migrations
+				execute :chmod, "-R 777 storage"
+				# execute :php, "artisan migrate" # run migrations
             end
         end
     end
